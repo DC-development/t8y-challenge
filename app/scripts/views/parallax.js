8aa7d4fcdd6868e72c04108e010accd359f13c69
philipp.sengelmann <sengelmann.philipp@googlemail.com>
2014-12-02 22:09:49 +0100

    parallax script moved to render section for parallax view

--- app/scripts/views/parallax.js
+++ app/scripts/views/parallax.js
@@ -18,98 +18,109 @@ define([
         className: '',
 
         events: {},
-        
+
         render: function () {
             this.$el.html(this.template({}));
         }
     });
 
-    $(document).ready(function() {
+    /**
+     * Parallax-effect beta
+     *
+     * Tryin to copy the parallax effect from this page:
+     * // http://emosite.volkswagen.de/jetta/?culture=de_DE
+     *
+     * @Author: Philipp Sengelmann <sengelmann.philipp@googlemail.com>
+     *
+     */
+
+    $(document).ready(function () {
 
         var $window = $(window);
 
         //Vars for calculating focussed objects position    
-        var imgHeight = 540; 
+        var imgHeight = 540;
         var inertia = 4; //Alter this value to adjust the slow-down effect
         var parkHeight = imgHeight / inertia;
         var parkStep = parkHeight / imgHeight;
 
-        $.fn.scrollEnd = function(callback, timeout) {
-            $(this).scroll(function(){ 
+
+
+        $.fn.scrollEnd = function (callback, timeout) {
+            $(this).scroll(function () {
                 var $this = $(this);
                 if ($this.data('scrollTimeout')) {
                     clearTimeout($this.data('scrollTimeout'));
                 }
-                $this.data('scrollTimeout', setTimeout(callback,timeout));
-            }); 
+                $this.data('scrollTimeout', setTimeout(callback, timeout));
+            });
         };
-       
-        $(window).scrollEnd(function(){
- 
+
+        $(window).scrollEnd(function () {
+
             var parallaxContent = $(".parallax-content.in-focus");
             var parentContainer = $(".parallax-content.in-focus").parent();
-            
-            //var yPos = Math.floor(parallaxContent.offset().top - $window.scrollTop());
-            var yPosAbsolute = Math.floor(parentContainer.offset().top); 
-            
+
+            var yPosAbsolute = Math.floor(parentContainer.offset().top);
             var yPosNextSibling = Math.floor(parentContainer.next().offset().top - $window.scrollTop());
             var yPosNextSiblingAbsolute = Math.floor(parentContainer.next().offset().top);
-             
-            if(yPosNextSibling < 200){
-                //window.scrollTo(0,yPosNextSiblingAbsolute);
+
+            if (yPosNextSibling < 200) {
+
                 var body = $("html, body");
-                body.animate({scrollTop:yPosNextSiblingAbsolute}, '500', 'swing', function() {
-                   // alert("Finished animating"); 
-                    var block = false;
+                body.animate({
+                    scrollTop: yPosNextSiblingAbsolute
+                }, '500', 'swing', function () {
+
                 });
-            }else{
+            } else {
                 var body = $("html, body");
-                body.animate({scrollTop:yPosAbsolute}, '500', 'swing', function() {
-                   // alert("Finished animating");
-                    var block = false;
+                body.animate({
+                    scrollTop: yPosAbsolute
+                }, '500', 'swing', function () {
+
                 });
-            } 
-            
-            //console.log('stopped scrolling focused objects is at:'+ yPos+' and at absolute: '+ yPosAbsolute); 
-            //console.log('next to focused object is at:'+ yPosNextSibling +'and at absolute: '+ yPosNextSiblingAbsolute); 
+            }
         }, 500);
-   
-        $window.scroll(function() { 
-             
+
+        $window.scroll(function () {
             /**
              * Get all parallax containers within the function
              * @Todo: Would be better to fetch it on init once but the way the script doesnt fire in response on backbone-route
              */
-            var parallaxContainers = $(".parallax-content"); 
- 
+            var parallaxContainers = $(".parallax-content");
+
+
             //This is called for every scrollstep 
-            $.each(parallaxContainers, function( index, value ) {
+            $.each(parallaxContainers, function (index, value) {
 
                 //fetch the current container from the array and wrap it as a jQuery-Object
                 var container = $(parallaxContainers[index]);
                 //To use it like this to get its parent offset cause we test for <0 and set the child 
-                //to top:0
+                //to top:0 while the parent container is beeing scrolled on, so we have access to the original position.
                 var yPos = Math.floor(container.parent().offset().top - $window.scrollTop());
-                var id = container.attr('id');
-                
-                if(yPos<=0&&yPos>-imgHeight){
-                    container.addClass("in-focus"); 
+                //
+                if (yPos <= 0 && yPos > -imgHeight) {
+                    container.addClass("in-focus");
                     var ps = Math.floor(container.parent().next().offset().top - $window.scrollTop());
                     var d = (imgHeight - ps) * parkStep;
-                    container.css('top', -d); 
+                    container.css('top', -d);
+                    container.css('opacity', ((1 / imgHeight) * ps));
                     //console.log(imgHeight +"-"+ ps +"*"+parkStep+"="+ d); 
-                }else if(container.css("position")=="fixed"){ 
+                } else if (container.css("position") == "fixed") {
                     container.removeClass("in-focus");
                     container.css('top', '');
+                } else {
+                    var ps = Math.floor(container.parent().next().offset().top - $window.scrollTop());
+                    container.css('opacity', ((1 * imgHeight) / ps));
                 }
-                 
-                //container.find(".pos").text(yPos +"-"+ ps); 
+
+                //container.find(".pos").text((1*imgHeight)/ps);
                 //console.log(p , id); 
             });
         });
 
     });
-    
+
     return ParallaxView;
 });
-
